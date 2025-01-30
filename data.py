193799ed27e9ca2e6ca4b4e75a5ad2c09e40d001
Irina Morgunova <i-morgunova@yandex.ru>
2025-01-30 22:21:38 +0300

    First_commit

---
+++ data.py
@@ -0,0 +1,9 @@
+name = "Краторная булка N-200i"
+price = 1255
+
+INGREDIENTS= [
+    ('SAUCE', 'Spicy-Х',90),
+    ('SAUCE', 'Spase Sauce',80),
+    ('FILLING','Protostomia',1337),
+    ('FILLING','cheese',900)
+    ]
